69f2a2d0114e9e63c83078be3a6c335347ca0a86
Jaime Garcia <garciar.jaime@gmail.com>
2015-05-14 20:33:07 -0400

    Initial front-end
    This includes the initial setup for the front-end, including AngularJS,
    Angular UI-Router, and the Pure CSS framework.

---
+++ src/main/resources/public/modules/starter.js
@@ -0,0 +1,21 @@
+/**
+ * Starter Application
+ * 
+ * This can be used as a template to create a starter UI for the application.
+ * This will configure states and their related views.
+ * 
+ * @author Jaime Garcia
+ */
+angular.module('starter', [ 'ui.router' ]).config(
+	[ '$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+		// this will set the default state to be /home
+		$urlRouterProvider.otherwise('/home')
+
+		$stateProvider.state('home', {
+			url: '/home',
+			templateUrl: '/modules/home/home.view.html'
+		}).state('about', {
+			url: '/about',
+			templateUrl: '/modules/about/about.view.html'
+		})
+	} ])
